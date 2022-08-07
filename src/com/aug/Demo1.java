51efb0f9e72e2b49cfa32e552f000c3ebc7fe431
sharathgitha627 <sharathgitha1627@gmail.com>
2022-08-07 10:17:53 +0530

    all lines deleted & new content added...

--- src/com/aug/Demo1.java
+++ src/com/aug/Demo1.java
@@ -3,16 +3,10 @@ package com.aug;
 public class Demo1 {
 	
 	public static void main(String[] args) {
-		
-		System.out.println("this is simple java class");
-		
-		System.out.println("this is sharath");
-		
-		
-		System.out.println("this is 12th line.....");
-		
-		
-		
+		System.out.println("this is first line....");
+		System.out.println("this is second line....");
+		System.out.println("this is third line....");
+		System.out.println("this is fourth line.....");
 		
 	}
 
